b0778bf287d091cdb2f20d21dec2954b7d1df7f4
psadhula <96182754+psadhula@users.noreply.github.com>
2022-01-12 23:32:11 +0530

    Create student_spec.js
    cm

---
+++ cypress/integration/features/student/student_spec.js
@@ -0,0 +1,16 @@
+
+import selector from '../../../support/elementSelectors/selectors'
+
+describe('Feature : Success Login for a Student', function(){
+
+
+
+it('Test to View Attendance Log',function(){
+    cy.log("Second Step : Verify Welcome Message!!");
+    cy.VerifyWelcomeHeader();
+    
+})
+
+
+
+})
